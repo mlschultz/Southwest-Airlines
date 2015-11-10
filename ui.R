f5aaad528955112f7e798da87b0c326f70ca53d3
mlschultz <m.l.schultz@eagle.clarion.edu>
2015-11-10 15:48:38 -0500

    Initial files

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(dygraphs)
+
+shinyUI(fluidPage(
+  titlePanel("Carry Trade Profit"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Select Funding and Investing Currency"),
+      
+      selectizeInput("funding", label = h3("Select Funding Currency"), choices= c("JPY" = "1", "GBP" = "2"), selected = "JPY") ,
+      
+      selectizeInput("investing", label = h3("Select Investing Currency"), choices= c("JPY" = "1", "GBP" = "2"), selected = "GBP") 
+      
+      ## TODO: Input a date range input so you can specify the carry trade interval ----
+    ),
+    
+    ### uncomment for static chart    
+    ## mainPanel(plotOutput("plot"))
+    
+    ### uncomment for dygraphs chart
+    mainPanel(dygraphOutput("plot"))
+  )
+))
